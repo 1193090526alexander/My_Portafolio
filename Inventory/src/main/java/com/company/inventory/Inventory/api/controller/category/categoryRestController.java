30cb4b3636cec128278bd6da0eed668046a07a4a
Helmer Alexander Patiño <80782571+1193090526alexander@users.noreply.github.com>
2026-04-28 02:01:39 -0500

    ok

--- Inventory/src/main/java/com/company/inventory/Inventory/api/controller/category/categoryRestController.java
+++ Inventory/src/main/java/com/company/inventory/Inventory/api/controller/category/categoryRestController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
+@CrossOrigin(origins = {"http://localhost:4200"})
 public class categoryRestController {
 
     @Autowired
