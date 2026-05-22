4d1e8d721bc909289609a6375dddf036cb12db39
Helmer Alexander Patiño <80782571+1193090526alexander@users.noreply.github.com>
2026-05-22 16:15:33 -0500

    deleted de forn end

---
+++ Inventory/src/main/java/com/company/inventory/Inventory/api/services/category/ICategoryService.java
@@ -0,0 +1,15 @@
+package com.company.inventory.Inventory.api.services.category;
+
+import com.company.inventory.Inventory.api.model.CategoryEntity;
+import com.company.inventory.Inventory.api.response.category.CategoryResposeRest;
+import org.springframework.http.ResponseEntity;
+
+public interface ICategoryService {
+
+     ResponseEntity<CategoryResposeRest> serch();
+     ResponseEntity<CategoryResposeRest> findByName(String name);
+     ResponseEntity<CategoryResposeRest> findById(Integer id);
+     ResponseEntity<CategoryResposeRest> deleteCategoria(Integer id);
+     ResponseEntity<CategoryResposeRest> saveCategory(CategoryEntity category);
+     ResponseEntity<CategoryResposeRest> updateCategory(CategoryEntity category, Integer id);
+}
