a6c03da46ea0e4c8ceadf0a7766979f4dfcf64e3
Helmer Alexander Patiño <80782571+1193090526alexander@users.noreply.github.com>
2026-05-26 23:36:55 -0500

    Product- Finished Consult by id

--- Inventory/src/main/java/com/company/inventory/Inventory/api/controller/category/ProductRestController.java
+++ Inventory/src/main/java/com/company/inventory/Inventory/api/controller/category/ProductRestController.java
@@ -2,6 +2,7 @@ package com.company.inventory.Inventory.api.controller.category;
 
 
 import com.company.inventory.Inventory.api.model.ProductEntity;
+import com.company.inventory.Inventory.api.response.category.CategoryResposeRest;
 import com.company.inventory.Inventory.api.response.product.ProductoResponseRest;
 import com.company.inventory.Inventory.api.services.product.IProductService;
 import com.company.inventory.Inventory.api.util.Util;
@@ -37,5 +38,11 @@ public class ProductRestController {
         productEntity.setPicture(Util.compressZLib(picture.getBytes()));
         ResponseEntity<ProductoResponseRest> response = productService.saveProduct(productEntity, category);
         return response;
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<ProductoResponseRest>  searchCategory(@PathVariable Long id){
+        ResponseEntity<ProductoResponseRest> responseEntity = productService.findById(id);
+        return responseEntity;
     }
 }
